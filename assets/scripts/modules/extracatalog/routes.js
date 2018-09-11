1044114b5dd6380e51e53d1c90f981c65fc6d062
Víctor Valencia <valencia6x@gmail.com>
2018-09-11 10:55:31 -0500

    add url vars

--- assets/scripts/modules/extracatalog/routes.js
+++ assets/scripts/modules/extracatalog/routes.js
@@ -3,6 +3,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Index from './components/Index';
 import Voyage from '../voyage/Index';
 
+var pathname1 = window.location.pathname; // Returns path only
+console.log('pathname1',pathname1);
+var url1      = window.location.href;     // Returns full URL
+console.log('url1',url1);
+var origin1   = window.location.origin;   // Returns base URL
+console.log('origin1',origin1);
 console.log('siteURL',experiensa_vars.siteurl);
 let siteUrl = experiensa_vars.siteurl.replace('http://','')+'/extra-catalogue/';
 siteUrl = siteUrl.replace('localhost', '');
